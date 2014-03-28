bca81a8c6855f4e06ac4eb6c5ad711a3ab50e46d
nathantschultz <nathantschultz@gmail.com>
2014-03-28 11:52:04 -0600

    searching twitter!

--- MovieTweets/src/MovieTweets/TwitterSearch.java
+++ MovieTweets/src/MovieTweets/TwitterSearch.java
@@ -1,13 +1,19 @@
 package MovieTweets;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import twitter4j.*;
-import java.util.List;
+
+
+
+
 
 /**
  * Servlet implementation class TwitterSearch
@@ -15,7 +21,36 @@ import java.util.List;
 @WebServlet("/TwitterSearch")
 public class TwitterSearch extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	
+	
+	
+	
+	public static void search(String string) {
+        if (string.length() < 1) {
+            System.out.println("java twitter4j.examples.search.SearchTweets [query]");
+            System.exit(-1);
+        }
+        Twitter twitter = new TwitterFactory().getInstance();
+        try {
+            Query query = new Query(string);
+            QueryResult result;
+            do {
+                result = twitter.search(query);
+                List<Status> tweets = result.getTweets();
+                for (Status tweet : tweets) {
+                    System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
+                }
+            } while ((query = result.nextQuery()) != null);
+            System.exit(0);
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to search tweets: " + te.getMessage());
+            System.exit(-1);
+        }
+    }
+	
+	
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -24,59 +59,13 @@ public class TwitterSearch extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-    
-    
-
-    
-	
-	
-	
-	public class SearchTweets {
-	    /**
-	     * Usage: java twitter4j.examples.search.SearchTweets [query]
-	     *
-	     * @param args search query
-	     */
-	    public static void main(String[] args) {
-	        if (args.length < 1) {
-	            System.out.println("java twitter4j.examples.search.SearchTweets [query]");
-	            System.exit(-1);
-	        }
-	        Twitter twitter = new TwitterFactory().getInstance();
-	        try {
-	            Query query = new Query(args[0]);
-	            QueryResult result;
-	            do {
-	                result = twitter.search(query);
-	                List<Status> tweets = result.getTweets();
-	                for (Status tweet : tweets) {
-	                    System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
-	                }
-	            } while ((query = result.nextQuery()) != null);
-	            System.exit(0);
-	        } catch (TwitterException te) {
-	            te.printStackTrace();
-	            System.out.println("Failed to search tweets: " + te.getMessage());
-	            System.exit(-1);
-	        }
-	    }
-	}
-	
-	
-    
-    
-    
-    
-    
-    
-    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		search("movies");
 	}
-
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
